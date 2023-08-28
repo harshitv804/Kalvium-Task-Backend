49fc810cd2c7a2a2989a16d2568e7c9613d2582a
Harshit V <100853494+harshitv804@users.noreply.github.com>
2023-08-28 14:44:28 +0530

    Added database to save history

--- backend.py
+++ backend.py
@@ -3,36 +3,67 @@
 # SRM Institute Of Science And Technology Ramapuram
 
 from flask import Flask, request, jsonify
+import sqlite3
+
+# Initialize SQLite database
+conn = sqlite3.connect('history.db', check_same_thread=False)
+cursor = conn.cursor()
+
+# Create a table to store the history if it doesn't exist
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS history (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        question TEXT NOT NULL,
+        answer TEXT NOT NULL
+    )
+''')
 
 app = Flask(__name__)
 
-cal_history = []
-
+# Helper function to perform mathematical operations
 def calculate_expression(expression):
     try:
-        return str(eval(expression)) 
+        return str(eval(expression))
     except:
         return "Invalid expression"
 
 @app.route('/<path:expression>', methods=['GET'])
+
 def perform_operation(expression):
+    if expression == 'favicon.ico':
+        return ""
+    # Replace keywords with symbols
+    expression = expression.replace('plus', '+').replace('minus', '-').replace('into', '*').replace('dividedby', '/')
+
+    # Split the expression by '/'
     parts = expression.split('/')
 
+    # Build the math expression from the URL
     math_expression = ''.join(parts)
 
+    # Calculate the result
     result = calculate_expression(math_expression)
 
-    # Add the operation to the history
-    cal_history.append({"question": math_expression, "answer": result})
-
-    if len(cal_history) > 20:
-        cal_history.pop(0)
-
+    # Insert the operation into the database
+    cursor.execute('INSERT INTO history (question, answer) VALUES (?, ?)', (math_expression, result))
+    cursor.execute('''
+        DELETE FROM history 
+        WHERE id = (SELECT id FROM history ORDER BY id ASC LIMIT 1)
+        AND (SELECT COUNT(*) FROM history) > 20
+    ''')
+    conn.commit()
     return jsonify({"question": math_expression, "answer": result})
 
 @app.route('/history', methods=['GET'])
 def get_history():
-    return jsonify(cal_history)
+    # Retrieve the history from the database
+    cursor.execute('SELECT question, answer FROM history ORDER BY id DESC LIMIT 20')
+    history_from_db = cursor.fetchall()
+    
+    # Convert the result to a list of dictionaries
+    history_list = [{"question": row[0], "answer": row[1]} for row in history_from_db]
+
+    return jsonify(history_list)
 
 if __name__ == '__main__':
     app.run(debug=True)
